78174c99d9e7c561a6e7c7de9d90ac5375ff3925
guochen <guochen@zhuoyuan-info.com>
2019-05-06 15:18:39 +0800

    add example3

--- src/main/java/com/zy/netty/example3/MyChatInitializer.java
+++ src/main/java/com/zy/netty/example3/MyChatInitializer.java
@@ -1,13 +1,10 @@
 package com.zy.netty.example3;
 
-import com.zy.netty.example2.MyClientHandler;
-
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
-import io.netty.handler.codec.LengthFieldPrepender;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
@@ -19,11 +16,10 @@ public class MyChatInitializer extends ChannelInitializer<SocketChannel>{
 		
 		ChannelPipeline pipeline = ch.pipeline();
 		
-		pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
-		pipeline.addLast(new LengthFieldPrepender(4));
+		pipeline.addLast(new DelimiterBasedFrameDecoder(4096,Delimiters.lineDelimiter()));
 		pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
 		pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
-		pipeline.addLast(new MyClientHandler());
+		pipeline.addLast(new MyChatServerHandler());
 	}
 
 }
