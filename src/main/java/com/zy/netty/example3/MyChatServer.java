acb45a761cb4f21f1240585ec4de4b95b4c2e063
guochen <guochen@zhuoyuan-info.com>
2019-05-06 10:24:57 +0800

    commit

--- src/main/java/com/zy/netty/example3/MyChatServer.java
+++ src/main/java/com/zy/netty/example3/MyChatServer.java
@@ -1,5 +1,25 @@
 package com.zy.netty.example3;
 
-public class MyChatServer {
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+public class MyChatServer {
+	public static void main(String[] args) throws Exception {
+		// 事件循环组
+			EventLoopGroup parentLoopGroup = new NioEventLoopGroup();
+			EventLoopGroup childLoopGroup = new NioEventLoopGroup();
+			try {
+				ServerBootstrap serverBootstrap = new ServerBootstrap();
+				serverBootstrap.group(parentLoopGroup, childLoopGroup).channel(NioServerSocketChannel.class).childHandler(new MyChatInitializer());
+				
+				ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+				channelFuture.channel().closeFuture().sync();
+			} finally {
+				parentLoopGroup.shutdownGracefully();
+				childLoopGroup.shutdownGracefully();
+			}
+	}
 }
