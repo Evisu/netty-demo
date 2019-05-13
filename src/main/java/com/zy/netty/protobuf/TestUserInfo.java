a16749977e2c90ca3bbe0fce850ee3c74f9d2d41
guochen <guochen@zhuoyuan-info.com>
2019-05-13 10:37:12 +0800

    定义了protobuf文件并测试protobuf生成的对象

--- src/main/java/com/zy/netty/protobuf/TestUserInfo.java
+++ src/main/java/com/zy/netty/protobuf/TestUserInfo.java
@@ -2,7 +2,17 @@ package com.zy.netty.protobuf;
 
 public class TestUserInfo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		
+		UserInfo.User user = UserInfo.User.newBuilder().setName("张三").setAddress("劳动南路").setAge(20).build();
+		
+		byte[] userByteArray = user.toByteArray();
+		
+		UserInfo.User user2 = UserInfo.User.parseFrom(userByteArray);
+		
+		System.out.println(user2.getName());
+		System.out.println(user2.getAge());
+		System.out.println(user2.getAddress());
 	}
 
 }
